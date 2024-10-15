701820de13eb4c0b388f1c4d00ba97774ca54eb5
Naveez Munir <naveezmunir@gmail.com>
2024-10-15 10:00:00 +0500

    Update leave index route

--- app/routes/dashboard/leave/index.tsx
+++ app/routes/dashboard/leave/index.tsx
@@ -2,23 +2,33 @@ import { useParams } from 'react-router'
 import type { Route } from "../+types";
 import { ExpenseSection } from '~/components/account/expense/ExpenseSection';
 import { LeaveSection } from '~/components/leave/staff/LeaveSection';
+import { AllStudentLeavesTable } from '~/components/leave/student';
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "Daily diary Management" },
-    { name: "description", content: "Manage school Daily Diary" },
+    { title: "Leave Management" },
+    { name: "description", content: "Manage staff and student leaves" },
   ];
 }
 
 export default function Leave() {
-  const {action, type } = useParams();
+  const { action } = useParams();
+
+  if (!action) {
+    return <LeaveSection />;
+  }
+
   if (action === "staff") {
     return <LeaveSection />;
   }
+
+  if (action === "student") {
+    return <AllStudentLeavesTable />;
+  }
+
   if (action === "expenses") {
     return <ExpenseSection />;
   }
-  return <div>
-    <h1> Leave management </h1>
-  </div>;
+
+  return <LeaveSection />;
 }
