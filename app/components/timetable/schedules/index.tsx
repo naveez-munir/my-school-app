09e01be976e0a7f04ea80d375f47ab1c725b880c
Naveez Munir <naveezmunir@gmail.com>
2024-12-12 10:00:00 +0500

    Add index.tsx

---
+++ app/components/timetable/schedules/index.tsx
@@ -0,0 +1,4 @@
+export { ScheduleGrid } from './ScheduleGrid';
+export { StudentScheduleView } from './StudentScheduleView';
+export { TeacherScheduleView } from './TeacherScheduleView';
+export { GuardianScheduleView } from './GuardianScheduleView';
