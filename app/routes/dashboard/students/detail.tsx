32330add68abcb4a46cb271f779eebdffc626f70
Naveez Munir <naveezmunir@gmail.com>
2025-05-01 16:29:11 +0500

    added route changes for student profile

--- app/routes/dashboard/students/detail.tsx
+++ app/routes/dashboard/students/detail.tsx
@@ -3,6 +3,9 @@ import { useParams } from "react-router";
 import type { Route } from "../+types";
 import { StudentDetailPage } from "~/components/student/StudentDetailPage";
 import { StudentLeavesTable } from "~/components/leave/student/StudentLeavesList";
+import { StudentDailyDiaryDashboard } from "~/components/dailyDiary/StudentDailyDiary";
+import StudentExamDashboard from "~/components/examSection/exams/StudentExamDashboard";
+import StudentResultDashboard from "~/components/examSection/examResult/StudentResultDashboard";
 
 export function meta({ params }: Route.MetaArgs) {
   return [
@@ -15,6 +18,15 @@ export default function StudentDetail() {
   const { id } = useParams();
   if (id === 'leave') {
     return <StudentLeavesTable />
+  }
+  if (id === 'dairy') {
+    return <StudentDailyDiaryDashboard />
+  }
+  if(id === 'exams') {
+    return <StudentExamDashboard />
+  }
+  if(id === 'result') {
+    return <StudentResultDashboard />
   }
   return <StudentDetailPage />;
 }
