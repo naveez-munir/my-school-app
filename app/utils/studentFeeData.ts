073cb26a1666d544a619ee142006300955d73015
Naveez Munir <naveezmunir@gmail.com>
2025-06-30 12:00:00 +0500

    added student fee data util

---
+++ app/utils/studentFeeData.ts
@@ -0,0 +1,21 @@
+export const monthOptions = {
+  '1': 'January',
+  '2': 'February',
+  '3': 'March',
+  '4': 'April',
+  '5': 'May',
+  '6': 'June',
+  '7': 'July',
+  '8': 'August',
+  '9': 'September',
+  '10': 'October',
+  '11': 'November',
+  '12': 'December'
+};
+
+export const quarterOptions = {
+  '1': 'Quarter 1',
+  '2': 'Quarter 2',
+  '3': 'Quarter 3',
+  '4': 'Quarter 4'
+};
