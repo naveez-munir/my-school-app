85b373a80f43866b0708840398bc228019222a8f
Naveez Munir <naveezmunir@gmail.com>
2026-02-09 12:48:00 +0500

    Update api.ts utility

--- app/services/api.ts
+++ app/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { getAuthData } from '~/utils/auth';
 
-const BASE_URL = 'http://localhost:3000';
+const BASE_URL = 'https://myschool-api.totaltravelsolutiongroup.com';
 
 const api = axios.create({
   baseURL: BASE_URL,
